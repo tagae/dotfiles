d1e18deeb4115deed07aabaf15efb31363277ee4
Sebastián González <s.gonzalez@uclouvain.be>
2012-11-16 17:16:16 +0100

    Have Emacs correctly detect format of more files.

--- emacs.d/preload/conf-unix-mode.el
+++ emacs.d/preload/conf-unix-mode.el
@@ -1,1 +1,3 @@
 (add-to-list 'auto-mode-alist '("\.?inputrc\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("sshd_config\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("/pam.d/.+\\'" . conf-unix-mode))
