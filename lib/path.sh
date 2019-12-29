9db1ecf38809c662fef2d89c11a9d64a18e27a86
Sebastián González <tagae@ehub.io>
2019-12-29 01:03:49 +0000

    refactor(path): replace replace relative-to by from-home

--- lib/path.sh
+++ lib/path.sh
@@ -1,7 +1,10 @@
 test -v PATH_LIB && return || readonly PATH_LIB="${BASH_SOURCE[0]}"
 
 absolute-dirname() {
-    ( cd "$(dirname "$1")"; pwd; )
+    (
+        cd "$(dirname "$1")"
+        pwd
+    )
 }
 
 absolute-path() {
@@ -9,8 +12,18 @@ absolute-path() {
     echo "$(absolute-dirname "$file")/$(basename "$file")"
 }
 
-relative-to() {
-    local -r dir="$1" file="$2"
-    [[ "$file" =~ ^"$dir"/(.+) ]]
-    echo "${BASH_REMATCH[1]}"
+from-home() {
+    local -r path="$1"
+    local absolute
+    absolute="$(absolute-path "$path")"
+    if test "$absolute" = "$HOME"; then
+        echo '~'
+    else
+        local relative="${absolute##$HOME/}"
+        if test "$absolute" = "$relative"; then
+            echo "$path" # could not relativize
+        else
+            echo "~/$relative"
+        fi
+    fi
 }
