e0063396cb2052a66f31f2cd63a94e49fe70d483
Sebastian Gonzalez Montesinos <tagae@ehub.io>
2020-01-07 22:01:31 +0100

    feat: import xinitrc.d

---
+++ share/xinitrc.d/50-background.sh
@@ -0,0 +1,8 @@
+# Reset root window to default state.
+xsetroot
+
+# Set background color.
+# xsetroot -solid "#000000"
+
+# Pointer that does not obstruct what you click.
+# xsetroot -cursor_name left_ptr
